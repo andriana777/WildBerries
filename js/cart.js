8f4cb9a54542ab2078d5bd1468a808f64e0572b0
andriana777 <andrianaprokopets@gmail.com>
2021-11-20 22:30:42 +0200

    webpack added

---
+++ js/cart.js
@@ -0,0 +1,18 @@
+const cart = function() {
+const cartBtn = document.querySelector('.button-cart');
+const cart = document.getElementById('modal-cart');
+const closeBtn = document.querySelector('.modal-close')
+//cartBtn.onclick = function() {
+//console.log('click');
+//}
+console.log(cart)
+
+cartBtn.addEventListener('click', function() {
+    cart.style.display = 'flex'
+})
+
+closeBtn.addEventListener('click', function() {
+    cart.style.display = 'none'
+})
+}
+cart();
