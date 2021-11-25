71a0408dde108d90cc1d4fdbfaf6930274fec5aa
andriana777 <andrianaprokopets@gmail.com>
2021-11-25 23:32:40 +0200

    autoplay added

--- src/modules/slider.js
+++ src/modules/slider.js
@@ -1,9 +1,13 @@
-import Swiper, { Navigation, Pagination } from 'swiper';
-Swiper.use([Navigation, Pagination]);
+import Swiper, { Autoplay, Navigation, Pagination } from 'swiper';
+Swiper.use([Navigation, Pagination, Autoplay]);
 const slider = () => {
     console.log("Slider");
 const swiper = new Swiper(".swiper-container", {
     loop: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
     navigation: {
       nextEl: ".slider-button-next",
       prevEl: ".slider-button-prev",
